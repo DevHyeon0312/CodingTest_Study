3b79fc60711b19b2ca4fe2791b6d5fbbf841d1a7
devhyeon <devhyeon0312@gmail.com>
2020-11-11 10:28:22 +0900

    //Block

--- JavaSolution/src/Solution.java
+++ JavaSolution/src/Solution.java
@@ -2,21 +2,21 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Create Solution Project");
+        //System.out.println("Create Solution Project");
 
-        // Lv1TwoPlus.java
-        int[] lv1TwoPlusNumbers = {2,1,3,4,1};
-        int[] lv1TwoPlusResult = new Lv1TwoPlus().solution(lv1TwoPlusNumbers);
-        System.out.print("Lv1TwoPlus.java : Result = ");
-        for (int i : lv1TwoPlusResult) {
-            System.out.print(i+" ");
-        }
-        System.out.print("\n");
+        /** Lv1TwoPlus.java */
+//        int[] lv1TwoPlusNumbers = {2,1,3,4,1};
+//        int[] lv1TwoPlusResult = new Lv1TwoPlus().solution(lv1TwoPlusNumbers);
+//        System.out.print("Lv1TwoPlus.java : Result = ");
+//        for (int i : lv1TwoPlusResult) {
+//            System.out.print(i+" ");
+//        }
+//        System.out.print("\n");
 
-        // Lv1KakaoCrane.java
-        int[][] lv1KakaoCraneBoard = {{0,0,0,0,0}, {0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-        int[] lv1KakaoCraneMoves = {1,5,3,5,1,2,1,4};
-        int lv1KakaoCraneResult = new Lv1KakaoCrane().solution(lv1KakaoCraneBoard,lv1KakaoCraneMoves);
-        System.out.println("Lv1KakaoCrane.java : Result = "+lv1KakaoCraneResult);
+        /** Lv1KakaoCrane.java */
+//        int[][] lv1KakaoCraneBoard = {{0,0,0,0,0}, {0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+//        int[] lv1KakaoCraneMoves = {1,5,3,5,1,2,1,4};
+//        int lv1KakaoCraneResult = new Lv1KakaoCrane().solution(lv1KakaoCraneBoard,lv1KakaoCraneMoves);
+//        System.out.println("Lv1KakaoCrane.java : Result = "+lv1KakaoCraneResult);
     }
 }
