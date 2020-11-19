ffd1efba581fd25bd01fe7c2f8dd8bc84acc73b1
devhyeon <devhyeon0312@gmail.com>
2020-11-20 06:57:24 +0900

    <Lv1Interval.java> /** Created By DevHyeon on 2020.11.20  *  Question Title : 내적  *  Algorithm Type : Implementation  *  Level : 1  *  Link : https://programmers.co.kr/learn/courses/30/lessons/70128  * */

---
+++ JavaSolution/src/Lv1Internal.java
@@ -0,0 +1,19 @@
+/** Created By DevHyeon on 2020.11.20
+ *  Question Title : 내적
+ *  Algorithm Type : Implementation
+ *  Level : 1
+ *  Link : https://programmers.co.kr/learn/courses/30/lessons/70128
+ * */
+public class Lv1Internal {
+    public class Solution {
+        public int solution(int[] a, int[] b) {
+
+            long temp = 0;
+            for (int size = 0 ; size < a.length; size++) {
+                temp += a[size] * b[size];
+            }
+            int answer = (int)temp;
+            return answer;
+        }
+    }
+}
