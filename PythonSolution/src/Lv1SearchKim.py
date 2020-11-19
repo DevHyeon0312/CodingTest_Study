7da9ca494c738c976f74e14b3b33e876e1f792f0
devhyeon <devhyeon0312@gmail.com>
2020-11-20 06:27:22 +0900

    <Lv1SearchKim.py> # Created By DevHyeon on 2020.11.20 #  Question Title : 서울에서 김서방 찾기 #  Algorithm Type : Implementation #  Level : 1 #  Link : https://programmers.co.kr/learn/courses/30/lessons/12919

---
+++ PythonSolution/src/Lv1SearchKim.py
@@ -0,0 +1,8 @@
+# Created By DevHyeon on 2020.11.20
+#  Question Title : 서울에서 김서방 찾기
+#  Algorithm Type : Implementation
+#  Level : 1
+#  Link : https://programmers.co.kr/learn/courses/30/lessons/12919
+def solution(seoul):
+    answer = '김서방은 ' + str(seoul.index('Kim')) + '에 있다'
+    return answer
