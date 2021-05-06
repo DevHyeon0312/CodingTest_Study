41c51af12d1b9bad55910312f98b6f6af2411a1d
devhyeon <devhyeon0312@gmail.com>
2021-05-06 13:10:17 +0900

    백준 11022 A+B - 11022_B3.kt

---
+++ KotlinSolution/src/main/kotlin/baekjoonSolution/11022_B3.kt
@@ -0,0 +1,24 @@
+package baekjoonSolution
+
+fun main() {
+    val T = readLine()!!.toInt()
+    for (i in 1 .. T) {
+        val (A,B) = readLine()!!.split(" ").map { it.toInt() }
+        println(Solution11022(i,A,B).print())
+    }
+}
+
+class Solution11022 constructor(private val index:Int, private val A:Int, private val B:Int) {
+    fun print() : String {
+        return StringBuilder()
+            .append("Case #")
+            .append(index)
+            .append(": ")
+            .append(A)
+            .append(" + ")
+            .append(B)
+            .append(" = ")
+            .append((A+B))
+            .toString()
+    }
+}
