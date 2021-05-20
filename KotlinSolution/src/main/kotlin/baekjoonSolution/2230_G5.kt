6bb35e9ce4bd4e48242e62eda387ac2b749847bb
devhyeon <devhyeon0312@gmail.com>
2021-05-20 15:50:23 +0900

    백준 2230 수 고르기 속도개선 : 956ms -> 468ms -> 384ms 956ms : 단순 이진탐색 468ms : 이전에 찾은 부분의 오른쪽배열만 이진탐색 384ms : 입출력을 Buffered , StringTokenizer 사용

--- KotlinSolution/src/main/kotlin/baekjoonSolution/2230_G5.kt
+++ KotlinSolution/src/main/kotlin/baekjoonSolution/2230_G5.kt
@@ -1,36 +1,60 @@
 package baekjoonSolution
 
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
 fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+
     /** N, M 입력 */
-    val (N,M) = readLine()!!.split(" ").map { it.toInt() }
+    val stBlack = StringTokenizer(br.readLine(), " ")
+    val N = stBlack.nextToken().toInt()
+    val M = stBlack.nextToken().toInt()
 
     /** Array 입력 */
     val array = IntArray(N)
     for (i in 0 until N) {
-        array[i] = readLine()!!.toInt()
+        array[i] = StringTokenizer(br.readLine()).nextToken().toInt()
     }
 
     /** 정렬: 내부적으로 퀵소트로 동작 */
     array.sort()
 
-    /** 배열 출력 */
-//    println(array.contentToString())
-
     /** 두수의 차이가 M 이상이면서 가장 적은 차이 구하기 */
-    var min: Int = 2000000001
+    var min: Int = Int.MAX_VALUE
+    var start = 0
     for (i in array) {
-        val index = array.lowerBound((i+M))
-        if (index == array.size) continue
+        val index = array.lowerBound(start, (i+M))
+        //차이가 M 이상이 없는 경우
+        if (index == array.size) {
+            break
+        }
+
+        //두 수 차이가 M 이면 중지
+        if(min == M) {
+            break
+        }
+
+        //차이가 M 이상이면서 min 보다 작은 경우
         if(array[index] - i < min) {
             min = array[index] - i
         }
+        start = index
     }
-    println(min)
+
+    bw.write(min.toString())
+    br.close()
+    bw.close()
 }
 
 /** target 보다 크거가 같은 수중 가장 작은 수 */
-fun IntArray.lowerBound(target: Int): Int {
-    var begin = 0
+fun IntArray.lowerBound(start:Int, target: Int): Int {
+    //M 이상인 경우 왼쪽의 배열은 더이상 M 이상이 될 수 없으므로 탐색 대상에서 제거
+    var begin = start
     var end: Int = this.size
 
     while (begin < end) {
