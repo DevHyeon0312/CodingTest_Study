66dca20415c189ba3e115c6fe3cacd1b4f893393
devhyeon <devhyeon0312@gmail.com>
2021-05-30 18:31:34 +0900

    피보나치 수 5

---
+++ KotlinSolution/src/main/kotlin/baekjoonSolution/10870_B2.kt
@@ -0,0 +1,16 @@
+package baekjoonSolution
+
+fun main() {
+    val dp = IntArray(21)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 1
+
+    val N = readLine()!!.toInt()
+
+    for (i in 3..N) {
+        dp[i] = dp[i-1]+dp[i-2]
+    }
+
+    println(dp[N])
+}
